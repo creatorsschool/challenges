267ff84e6d042ffa0943c1d4e835b534ddb85a8b
Zamith <zamith.28@gmail.com>
2015-07-02 12:47:17 +0100

    Updates specs

--- week-2/ruby/public_interface/bank_account_spec.rb
+++ week-2/ruby/public_interface/bank_account_spec.rb
@@ -1,6 +1,4 @@
-# Encoding: utf-8
-
-require "account"
+require_relative "account"
 
 describe BankAccount do
   let(:account) { BankAccount.new("Bruce Lee", "FR14-2004-1010-0505-0001-3M02-606", 200, "brucelit") }
@@ -18,19 +16,6 @@ describe BankAccount do
       expect(account.position).to eq 200
     end
   end
-
-  describe '#add_transaction (private)' do
-    it 'add transactions to your account' do
-      account.send(:add_transaction, 200)
-      expect(account.instance_variable_get(:@transactions).length).to eq 2
-    end
-
-    it 'update its position when a new transaction is made' do
-      account.send(:add_transaction, -200)
-      expect(account.position).to eq 0
-    end
-  end
-
 
   describe '#to_s' do
     it 'returns a string' do
